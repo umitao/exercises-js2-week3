c9415e851432d2ad3eff826a46b4b25b666d9dbc
carlossanchezheil1984 <carlos.sanchez.heil@gmail.com>
2020-05-06 17:33:50 +0200

    exercises updated

---
+++ InClass/C-fetch/exercise.js
@@ -0,0 +1,12 @@
+/*
+================
+
+Create an HTML file that uses this javascript file.
+The HTML file should contain a form with 2 inputs: (latitude and longitude), and a button with the text GET.
+When you click in the button, you should use fetch function to get information from the URL https://fcc-weather-api.glitch.me/api/current?lat=35&lon=160 with 
+user latitude and longitude.
+When you get the response from the server, print the current temperature in an <h3> marker.
+
+
+================
+*/
